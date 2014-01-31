7146f3e1d6a062a15f2c568189b9af95d79a48f2
David Gutierrez <dgutierr@redhat.com>
2014-01-31 11:23:29 +0100

    Moved to dashbuilder-website repo

--- website/_ext/haml/filters/asciidoc.rb
+++
@@ -1,6 +0,0 @@
-require 'haml'
-
-Haml::Filters.register_tilt_filter 'AsciiDoc'
-Haml::Filters::AsciiDoc.options[:safe] = :safe
-Haml::Filters::AsciiDoc.options[:attributes] ||= {}
-Haml::Filters::AsciiDoc.options[:attributes]['showtitle'] = ''
--- website/_ext/pipeline.rb
+++
@@ -1,6 +0,0 @@
-require File.expand_path '../haml/filters/asciidoc.rb', __FILE__
-
-Awestruct::Extensions::Pipeline.new do
-	helper Awestruct::Extensions::Partial
-	helper Awestruct::Extensions::Relative
-end
