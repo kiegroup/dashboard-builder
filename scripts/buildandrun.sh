7da357b7ea160e6716bfea6c93dee66de9a7941f
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2014-03-12 09:35:49 +0100

    changed -Dmaven.test.skip=true to -DskipTests

--- scripts/buildandrun.sh
+++ scripts/buildandrun.sh
@@ -9,7 +9,7 @@ else
   echo "------------------------------------------------------------------"
 
   cd ..
-  mvn clean install -P $1,jetty -Dmaven.test.skip=true
+  mvn clean install -P $1,jetty -DskipTests
 
   export MAVEN_OPTS="-Xms1024M -Xmx2048M -XX:MaxPermSize=512m -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005"
   cd modules/dashboard-showcase/
