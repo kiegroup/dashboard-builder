a9409b780e4997d1763d0a6611aa1bfc819317d2
David Gutierrez <dgutierr@redhat.com>
2014-12-04 12:09:50 +0100

    BZ-1167986 - [One-Off] Frame-busting not implemented in BPMS (disabled by default)

--- modules/dashboard-ui/dashboard-ui-core/src/main/java/org/jboss/dashboard/ui/controller/requestChain/ResponseHeadersProcessor.java
+++ modules/dashboard-ui/dashboard-ui-core/src/main/java/org/jboss/dashboard/ui/controller/requestChain/ResponseHeadersProcessor.java
@@ -15,6 +15,7 @@
  */
 package org.jboss.dashboard.ui.controller.requestChain;
 
+import org.apache.commons.lang.StringUtils;
 import org.jboss.dashboard.annotation.config.Config;
 import org.jboss.dashboard.ui.HTTPSettings;
 
@@ -35,6 +36,21 @@ public class ResponseHeadersProcessor extends AbstractChainProcessor {
     @Inject @Config("text/html")
     private String responseContentType;
 
+    @Inject @Config("false")
+    private boolean xssProtectionEnabled;
+
+    @Inject @Config("false")
+    private boolean xssProtectionBlock;
+
+    /** There are three possible values for the X-Frame-Options headers:<ul>
+     *  <li>DENY, which prevents any domain from framing the content.</li>
+     *  <li>SAMEORIGIN, which only allows the current site to frame the content.</li>
+     *  <li>ALLOW-FROM uri, which permits the specified 'uri' to frame this page. (e.g., ALLOW-FROM http://www.example.com) The ALLOW-FROM option is a relatively recent addition (circa 2012) and may not be supported by all browsers yet. BE CAREFUL ABOUT DEPENDING ON ALLOW-FROM. If you apply it and the browser does not support it, then you will have NO clickjacking defense in place.</li>
+     * </ul>
+     */
+    @Inject @Config("")
+    private String xFrameOptions;
+
     public boolean processRequest() throws Exception {
         HttpServletRequest request = getHttpRequest();
         HttpServletResponse response = getHttpResponse();
@@ -47,6 +63,14 @@ public class ResponseHeadersProcessor extends AbstractChainProcessor {
         response.setHeader("Last-Modified", sdf.format(new Date()) + " GMT");
         response.setHeader("Cache-Control", "no-cache, must-revalidate");
         response.setHeader("Pragma", "no-cache");
+
+        if (xssProtectionEnabled) {
+            if (xssProtectionBlock) response.setHeader("X-XSS-Protection", "1; mode=block");
+            else response.setHeader("X-XSS-Protection", "1");
+        }
+        if (!StringUtils.isBlank(xFrameOptions)) {
+            response.setHeader("X-FRAME-OPTIONS", xFrameOptions);
+        }
         if (useRefreshHeader) {
             response.setHeader("Refresh", java.lang.String.valueOf(request.getSession().getMaxInactiveInterval() + 61));
         }
