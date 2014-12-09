9f021d5211a64962c4edcd63fc400e6c1a982923
David Gutierrez <dgutierr@redhat.com>
2014-12-09 11:44:47 +0100

    Accept extra arguments

--- scripts/buildandrun.sh
+++ scripts/buildandrun.sh
@@ -9,7 +9,7 @@ else
   echo "------------------------------------------------------------------"
 
   cd ..
-  mvn clean install -P $1,jetty -DskipTests $2 $3 $4
+  mvn clean install -P $1,jetty  $2 $3 $4
 
   export MAVEN_OPTS="-Xms1024M -Xmx2048M -XX:MaxPermSize=512m -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005"
   cd modules/dashboard-showcase/
